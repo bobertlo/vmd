b43e18c75b54cfc6c8a0fbfea158627f4a5cba82
Robert Lowry <bobertlo@gmail.com>
2018-11-18 05:39:22 -0600

    vmdfmt: update error handling

--- vmdfmt/main.go
+++ vmdfmt/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"./markdown"
 )
 
 func main() {
 	buf, err := markdown.RenderFile("test.md")
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
 	}
 	os.Stdout.Write(buf)
 }
