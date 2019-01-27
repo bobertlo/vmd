61f9e18caa5e2aaaa4155ce9c0e5964c371f953a
Robert Lowry <bobertlo@gmail.com>
2019-01-27 04:31:52 -0600

    add frontend test

--- cmd/vmdfmt/vmdfmt_test.go
+++ cmd/vmdfmt/vmdfmt_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -17,5 +19,24 @@ func TestIsMarkdown(t *testing.T) {
 	f, _ = os.Stat("main.go")
 	if isMarkdownFile(f) {
 		t.Error("isMarkdownFile failed")
+	}
+}
+
+func TestProcessFile(t *testing.T) {
+	src, err := ioutil.ReadFile("../../internal/renderer/testfiles/README.md")
+	if err != nil {
+		t.Error("could not load README.md")
+	}
+
+	//out := new(bytes.Buffer)
+	var out = bytes.NewBuffer(nil)
+
+	err = processFile("../../internal/renderer/testfiles/README.md", nil, out)
+	if err != nil {
+		t.Error("processFile failed")
+	}
+
+	if bytes.Compare(src, out.Bytes()) != 0 {
+		t.Error("file mismatch")
 	}
 }
