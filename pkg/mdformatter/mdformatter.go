70a4cc7d3ecc8b19b97885eaf07268eb2e6502d6
Robert Lowry <bobertlo@gmail.com>
2019-01-29 01:01:16 -0600

    documentation

--- pkg/mdformatter/mdformatter.go
+++ pkg/mdformatter/mdformatter.go
@@ -4,16 +4,21 @@ import (
 	"github.com/bobertlo/vmd/internal/renderer"
 )
 
+// MDFormatter is a struct with methods to format markdown
 type MDFormatter struct {
 	render *renderer.Renderer
 }
 
+// New returns a new MDFormatter which wraps lines at a specified number
+// of columns
 func New(cols int) *MDFormatter {
 	f := &MDFormatter{}
 	f.render = renderer.New(cols)
 	return f
 }
 
+// RenderBytes renders a markdown []byte slice and returns the formatted
+// outout ([]byte, nil) or an error (nil, error)
 func (f *MDFormatter) RenderBytes(input []byte) ([]byte, error) {
 	return f.render.RenderBytes(input)
 }
